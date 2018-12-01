262552b06fa71d4a490d52145d4d2474245b3b6f
rahulbhatu <rahulbhatu1991@gmail.com>
2018-12-01 10:59:07 +0000

    trigger test

--- script.sh
+++ script.sh
@@ -3,4 +3,5 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This is to be removed"
+echo "This is triggered from github"
 echo "this was run automagically"
